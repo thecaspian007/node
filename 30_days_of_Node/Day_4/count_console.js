e0200cde05580237196bdb8d35cb8a4ad58cde20
sumeet.singh <sumeet.singh@idfy.com>
2024-08-01 20:11:24 +0530

    Day4

---
+++ 30_days_of_Node/Day_4/count_console.js
@@ -0,0 +1,9 @@
+//This code counts the score of remo , rj and 
+//default score which goes to none of them 
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.count('remo');
+console.count('remo');
+console.count('rj');
+console.count();
