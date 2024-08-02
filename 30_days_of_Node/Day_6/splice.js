955cf8e62f253386445326bf619ca80746ef0963
sumeet.singh <sumeet.singh@idfy.com>
2024-08-02 15:55:39 +0530

    Day_6, Day_7, Day_8, Day_9

---
+++ 30_days_of_Node/Day_6/splice.js
@@ -0,0 +1,9 @@
+arr = ['b','e','a','o','p','n','r'];
+console.log(arr);
+arr.splice(2,2,'rj','nodejs');
+console.log(arr);	
+
+arr2 = ['b','e','a','o','p','n','r'];
+console.log(arr2);
+arr2.splice(2,2);
+console.log(arr2);
---
+++ 30_days_of_Node/Day_8/package.js
@@ -0,0 +1,27 @@
+// Not for any use. Just a informative section where we have to learn about methods and their callbacks.
+
+var function_name_1 = function (parameters){
+						
+    Function code starts
+    ...				...
+    ...				...
+    Functionality
+    ...  			...
+    ...				...
+    Function ends here 
+    }
+var function_name_2 = function (parameters){
+
+        
+    Function code starts
+    ...				...
+    ...				...
+    Functionality
+    ...  			...
+    ...				...
+    Function ends here 				
+    }
+module.exports = {
+function_1 : function_name_1,
+function_2 : function_name_2
+}; 
